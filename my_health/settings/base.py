d2de38a1bf51dfe19dc28ae3e28f53a6175ea76c
jeonghoonyu <moning02004@naver.com>
2022-06-26 16:44:33 +0900

    test code 를 위한 authentication class 추가

--- my_health/settings/base.py
+++ my_health/settings/base.py
@@ -119,6 +119,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
