25f6f1027665ad21d93db7fb1b668d6f24d6380a
jeonghoonyu <moning02004@naver.com>
2022-07-06 07:26:24 +0900

    add workout model

--- app_workout/models.py
+++ app_workout/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Workout(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    representation = models.ForeignKey("self", on_delete=models.CASCADE, null=True)
+    status = models.CharField(max_length=3, default="REG")
+    user = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
