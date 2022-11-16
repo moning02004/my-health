69dc66a0f1224f1eadb859800f90a491cfc01695
Yu Jeonghoon <moning02004@naver.com>
2022-11-16 10:57:51 +0900

    prefetch_related 추가

--- app_workout/tests.py
+++ app_workout/tests.py
@@ -1,10 +1,10 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
 from app_user.models import User
 from app_workout.models import Part, Workout
 
 
-class WorkoutTestCase(TestCase):
+class WorkoutTestCase(APITestCase):
     def setUp(self) -> None:
         user = User.objects.create(username="test")
         user.set_password("test")
@@ -13,11 +13,11 @@ class WorkoutTestCase(TestCase):
         part2 = Part.objects.get(name="가슴")
         part3 = Part.objects.get(name="팔")
 
-        self.workout1 = Workout.objects.create(name="랫풀 다운", description="등 운동입니다.")
+        self.workout1 = Workout.objects.create(name="랫풀 다운", description="등 운동입니다.", status="PUB")
         self.workout1.effective_part.add(part1)
         self.workout1.effective_part.add(part3)
 
-        workout2 = Workout.objects.create(name="벤치프레스", description="가슴 운동입니다.")
+        workout2 = Workout.objects.create(name="벤치프레스", description="가슴 운동입니다.", status="PUB")
         workout2.effective_part.add(part2)
         self.client.login(username="test", password="test")
 
@@ -47,9 +47,9 @@ class WorkoutTestCase(TestCase):
         self.assertEqual(len(response.data["effective_part"]), 2)
 
     def test_partial_update_workout(self):
-        test_text = "등 운동이며 협응근은 이두 입니다."
+        test_text = "등 운동이며 협응근은 이두입니다."
         response = self.client.patch(f"/workout/{self.workout1.id}", data={
             "description": test_text
-        }, content_type="application/json")
+        })
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data["description"], test_text)
